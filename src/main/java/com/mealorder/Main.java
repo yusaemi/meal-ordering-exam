2e69ea54d0bfb90853ad611b3a154895b4c981c5
yusaemi <dokodemoemi@gmail.com>
2025-11-12 15:40:30 +0800

    🧱 Initial structure

---
+++ src/main/java/com/mealorder/Main.java
@@ -0,0 +1,17 @@
+package com.mealorder;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // TODO: 實作主程式邏輯
+
+        System.out.println("=== 餐點統計 ===");
+        System.out.println("已成功配置的輕食餐數量: 0");
+        System.out.println("已成功配置的豐盛餐數量: 0");
+        System.out.println("=== 訂單號碼統計 ===");
+        System.out.println("產生的訂單號碼數量: 0");
+        System.out.println("已實際使用的訂單號碼數量: 0");
+    }
+
+}
---
+++ src/main/java/com/mealorder/model/LightMeal.java
@@ -0,0 +1,5 @@
+package com.mealorder.model;
+
+public class LightMeal {
+    // TODO: 實作輕食餐的相關屬性與方法
+}
